c4b16795662117250d4443ef191a397fa12a1608
Serkan Aydin <serayd61@hotmail.com>
2025-10-06 09:54:39 +0200

    Move Farcaster manifest to .well-known and update to next version format

--- learnframe-app/next.config.js
+++ learnframe-app/next.config.js
@@ -1,37 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  
-  images: {
-    domains: ['learnframe.vercel.app'],
-    formats: ['image/avif', 'image/webp'],
-  },
-  
   async headers() {
     return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL',
-          },
-        ],
-      },
       {
         source: '/.well-known/farcaster.json',
         headers: [
           {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Content-Type', 
+            key: 'Content-Type',
             value: 'application/json',
           },
           {
             key: 'Cache-Control',
-            value: 'public, max-age=3600',
+            value: 'public, max-age=0, must-revalidate',
           },
         ],
       },
